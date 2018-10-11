4755c60028a471e11a1ef2df54ffdb08682a6fc2
Will Barnes <wtb2@rice.edu>
2018-10-11 12:28:37 -0500

    Cleanup

--- globus-task-scheduler/__init__.py
+++ globus-task-scheduler/__init__.py
@@ -10,24 +10,6 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
 # Create scheduler
 scheduler = TransferScheduler()
-#scheduler.start()
-
-## Dummy jobs
-#def hello_world():
-#    return 'Hello World!'
-#def foo_bar():
-#    return 'Foo Bar!'
-#scheduler.add_job('hello world', hello_world, trigger='interval', minutes=5)
-#scheduler.add_job('foo bar', foo_bar, trigger='date',
-#                  run_date=datetime.datetime.now()+datetime.timedelta(days=1))
-# future_date = datetime.datetime.now() + datetime.timedelta(days=1)
-# scheduler.add_date_job(
-#     future_date,
-#     '4e3c15d8-6f53-11e8-9327-0a6d4e044368',
-#     '944ef3d8-912a-11e8-9674-0a6d4e044368',
-#     [{'source': '/storage-home/w/wtb2/data/bundle_heating_model/emission_model.json',
-#       'target': '/Users/willbarnes/Desktop/emodel.json'},],
-# )
 
 
 class Config(object):
@@ -43,28 +25,17 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(Config())
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
     # Register blueprints
     from . import home, transfers, tasks
     app.register_blueprint(home.bp)
     app.register_blueprint(transfers.bp)
     app.register_blueprint(tasks.bp)
-
     # Register scheduler
-    #scheduler = TransferScheduler()
     scheduler.init_app(app)
     scheduler.start()
 
